4f28e7bd40e364c53301225a7c85d33903d4dc46
tssule3 <tssule3@gmail.com>
2021-12-18 19:36:34 +0530

    Added files to dev_branch

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angular-deploy-app');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-deploy-app app is running!');
-  });
 });
